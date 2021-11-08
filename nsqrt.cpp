34e3a58028dd72c4be6e1b5481fb8f1bab6ba21e
UnMondodiBonobo <alessandro.profera@gmail.com>
2021-11-08 02:26:00 +0100

    Square Root Newton Linear Aproximation

---
+++ nsqrt.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "nsqrt.h"
+#include "math.h"
+
+double nsqrt(double x, double epsilon) 
+{
+	double s = x / 2;
+
+	do 
+	{
+
+		s = ((s * s) + x) / (2 * s);
+
+	} while (abs((s*s)-x) >= epsilon);
+
+	return s;
+
+}
